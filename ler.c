d4edfc6026452a27db7c6bbf0b3cf0a01ee057cb
Pedro H. S. Mietlicki <pedromietlicke@gmail.com>
2022-08-11 10:45:20 -0300

    procura a melhor media de velocidade

--- ler.c
+++ ler.c
@@ -1,48 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
-int numeroDeLinhas(FILE *arquivo);
-int lerInt(char *mensagem){
+int lerInt(char *mensagem)
+{
     int valor = 0;
 
-    printf("%s \n",mensagem);
-    scanf("%d",&valor);
-    
+    printf("%s \n", mensagem);
+    scanf("%d", &valor);
+
     return valor;
 }
-float lerFloat(char *mensagem){
+float lerFloat(char *mensagem)
+{
     float valor = 0;
 
-    printf("%s \n",mensagem);
-    scanf("%f",&valor);
-    
+    printf("%s \n", mensagem);
+    scanf("%f", &valor);
+
     return valor;
 }
 
-int lerArquivo(float *trajeto){
+int lerArquivo(float *trajeto)
+{
     FILE *arquivo;
-    arquivo = fopen("hwycol.txt", "r");
+    arquivo = fopen("us06col.txt", "r");
     char tempo[50], velocidade[50];
     int i = 0;
     int linha = 0;
     rewind(arquivo);
-    while(fscanf(arquivo,"%s %s\n", &tempo, &velocidade) != EOF) {
-        *(trajeto + linha + 0) = (float) atof(tempo);
-        *(trajeto + linha +1 ) =  atof(velocidade) / 2.237;
+    while (fscanf(arquivo, "%s %s\n", &tempo, &velocidade) != EOF)
+    {
+        *(trajeto + linha + 0) = (float)atof(tempo);
+        // Converte milhas por hora para m/s
+        *(trajeto + linha + 1) = atof(velocidade) / 2.237;
         i++;
-        linha = i *3;
+        linha = i * 3;
         fflush(stdin);
     }
     fclose(arquivo);
 }
 
-int numeroDeLinhas(FILE *arquivo){
+int numeroDeLinhas()
+{
+    FILE *arquivo;
+    arquivo = fopen("us06col.txt", "r");
     int linhas = 0;
     char *linha[10];
     char chr = getc(arquivo);
-    while(chr != EOF){
-        if(chr == '\n')
+    while (chr != EOF)
+    {
+        if (chr == '\n')
             linhas++;
         chr = getc(arquivo);
     }
+    fclose(arquivo);
     return linhas;
 }
--- motor.c
+++ motor.c
@@ -1,66 +1,156 @@
 #include <math.h>
 #include <stdio.h>
-#define AreaFrontal 2.14
-#define CoeficienteDeArrasto 0.35
+#define AreaFrontal 2.16
+#define CoeficienteDeArrasto 0.38
 #define RendimentoTransmissao 0.94
 #define GravidadeTerrestre 9.81
 
-/* 
+/*
    0 = Tempo
    1 = Velocidade
    2 = Aceleração
    3 = Força resultante
-   4 = Força de arrasto 
+   4 = Força de arrasto
 
 */
 
 /**
  * @brief Retorna a aceleração para cada ponto do percurso.
- * 
+ *
  * @param trajeto
- * @param Tamanho 
+ * @param Tamanho
  */
-void pegaAceleracao(float *trajeto, int tamanho){
-    
-    
+void pegaAceleracao(float *trajeto, int tamanho)
+{
+
     int linha;
     float velocidade, vAnterior = 0, aceleracao;
-    for (int i = 0; i < tamanho; i++){
+    for (int i = 0; i < tamanho; i++)
+    {
         linha = i * 3;
-         velocidade = (*trajeto + linha + 1);
+        velocidade = (*trajeto + linha + 1);
         aceleracao = velocidade - vAnterior;
         vAnterior = velocidade;
-        *(trajeto + linha +2) = aceleracao;
+        *(trajeto + linha + 2) = aceleracao;
     }
 }
 
-void pegaForcaResultante(float *trajeto,float *consumo, int massa, int rpm, float torque, int tamanho){
+void pegaForcaResultante(float *trajeto, float *consumo, int massa, int rpm, float torque, int tamanho)
+{
     int linha;
-    float aceleracao , velocidade , forcaArrasto, forcaResultante, PotenciaInstantanea;
-    float MelhorPotencia = (torque * (double) rpm * GravidadeTerrestre * 3.14) /60;
+    float aceleracao, velocidade, forcaArrasto, forcaResultante, PotenciaInstantanea;
+    float MelhorPotencia = (torque * (double)rpm * GravidadeTerrestre * 3.14) / 60;
     float ConsumoMelhorPotencia = MelhorPotencia / (0.33 * 29500);
-    float forcaAtrito = 0.01 * (float) massa * GravidadeTerrestre;
-    for(int i = 0; i < tamanho; i++){
+    float forcaAtrito = 0.01 * (float)massa * GravidadeTerrestre;
+    for (int i = 0; i < tamanho; i++)
+    {
         linha = i * 3;
-        velocidade = *(trajeto + linha +1);
-        forcaArrasto = 0.5 * 1.20 * CoeficienteDeArrasto * AreaFrontal * pow(velocidade,2);
-        aceleracao =  *(trajeto + linha + 2);
+        velocidade = *(trajeto + linha + 1);
+        forcaArrasto = 0.5 * 1.20 * CoeficienteDeArrasto * AreaFrontal * pow(velocidade, 2);
+        aceleracao = *(trajeto + linha + 2);
 
         forcaResultante = (massa * aceleracao) > 0.2 ? (massa * aceleracao) + forcaAtrito + forcaArrasto : forcaAtrito + forcaArrasto;
-        *(trajeto + linha + 3 ) = forcaResultante;
-        PotenciaInstantanea =  forcaResultante * velocidade;
-        float consumoLPorHora = ConsumoMelhorPotencia * (0.15+ (1- 0.15) * PotenciaInstantanea / MelhorPotencia);
+        *(trajeto + linha + 3) = forcaResultante;
+        PotenciaInstantanea = forcaResultante * velocidade;
+        float consumoLPorHora = ConsumoMelhorPotencia * (0.15 + (1 - 0.15) * PotenciaInstantanea / MelhorPotencia);
         *(consumo + i) = (velocidade * 3.6) / consumoLPorHora;
     }
 }
-float mediaCombustivelGasto(float *consumo, int tamanho){
+float mediaCombustivelGasto(float *consumo, int tamanho)
+{
     float soma = 0;
-    for(int i = 0; i < tamanho; i++ ){
+    for (int i = 0; i < tamanho; i++)
+    {
         soma += *(consumo + i);
     }
     return soma / tamanho;
 }
+float distanciaPercorrida(float *trajeto, int tamanho)
+{
+    float percorrida = 0;
+    int linha;
+    for (int i = 0; i < tamanho; i++)
+    {
+        linha = i * 3;
+        percorrida += *(trajeto + linha + 1);
+    }
+    return percorrida;
+}
 
-int velocidade(){
-    return 0;
+void aumentaMedia(float *trajeto, int tamanho)
+{
+    int linha = 0;
+    float maior = 0;
+    for (int i = 0; i < tamanho; i++)
+    {
+        linha = i * 3;
+        maior = *(trajeto + linha + 1) > maior ? *(trajeto + linha + 1) : maior;
+    }
+
+    float menor = maior;
+
+    for (int i = 0; i < tamanho; i++)
+    {
+        linha = i * 3;
+        menor = (*(trajeto + linha + 1) < menor) && *(trajeto + linha + 1) != 0 ? *(trajeto + linha + 1) : menor;
+    }
+
+    for (int i = 0; i < tamanho; i++)
+    {
+        linha = i * 3;
+        if (*(trajeto + linha + 1) == menor)
+        {
+            *(trajeto + linha + 1) += 1;
+        }
+    }
+}
+void diminuiMedia(float *trajeto, int tamanho)
+{
+
+    float maior = 0;
+    int linha = 0;
+    for (int i = 0; i < tamanho; i++)
+    {
+        linha = i * 3;
+        maior = *(trajeto + linha + 1) > maior ? *(trajeto + linha + 1) : maior;
+    }
+    linha = 0;
+    for (int i = 0; i < tamanho; i++)
+    {
+        linha = i * 3;
+        if (*(trajeto + linha + 1) == maior)
+        {
+            *(trajeto + linha + 1) -= 1;
+        }
+    }
+}
+float calculaMediaVelocidade(float *trajeto, int tamanho)
+{
+    int linha;
+    float acumulador = 0;
+    for (int i = 0; i < tamanho; i++)
+    {
+        linha = i * 3;
+        acumulador += *(trajeto + linha + 1);
+    };
+    return acumulador / (float)tamanho;
+}
+float calculaVelocidade(float pontuacao, float *trajeto, int tamanho, float *consumo, int massa_do_carro, int rpm, float torque, float novaPontuacao, int sinal, float vInicial)
+{
+
+    sinal > 0 ? aumentaMedia(trajeto, tamanho) : diminuiMedia(trajeto, tamanho);
+    pegaAceleracao(trajeto, tamanho);
+    pegaForcaResultante(trajeto, consumo, massa_do_carro, rpm, torque, tamanho);
+    float novoConsumo = mediaCombustivelGasto(consumo, tamanho);
+    float novaDistancia = distanciaPercorrida(trajeto, tamanho);
+    novaPontuacao = novaDistancia / novoConsumo;
+    novaPontuacao = sinal > 0 ? novaPontuacao * 0.9 : novaPontuacao * 1.1;
+    if ((novaPontuacao <= pontuacao) && (calculaMediaVelocidade(trajeto, tamanho) > (vInicial - 2)))
+    {
+
+        calculaVelocidade(novaPontuacao, trajeto, tamanho, consumo, massa_do_carro, rpm, torque, novaPontuacao, sinal, vInicial);
+    }
+    else if (sinal > 0)
+        calculaVelocidade(novaPontuacao, trajeto, tamanho, consumo, massa_do_carro, rpm, torque, novaPontuacao, -1, vInicial);
+    return novoConsumo;
 }
